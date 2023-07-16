a7d60d059f245772538a23014086cf694054a68b
kentshenlim <kentshen_lim24@hotmail.com>
2023-07-16 22:38:16 +0100

    Add an img

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,11 +1,11 @@
-export default function Review(person, photo, quote) {
+export default function Review(person, quote, photo) {
   const reviewWrapper = document.createElement('div');
   reviewWrapper.classList.add('review-wrapper');
 
   const personNode = document.createElement('h3');
   personNode.textContent = person;
 
-  const quoteNode = document.createElement('q');
+  const quoteNode = document.createElement('blockquote');
   quoteNode.textContent = quote;
 
   reviewWrapper.appendChild(personNode);
