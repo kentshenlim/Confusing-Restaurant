92d4b9274e7ccc690836e8137676ec50be744f07
kentshenlim <kentshen_lim24@hotmail.com>
2023-07-19 22:16:42 +0100

    Tab logic is working

--- src/index.js
+++ src/index.js
@@ -5,6 +5,39 @@ import './style.css';
 import './background.css';
 import './scrollbar.css';
 
-const body = document.querySelector('body');
+(() => {
+  const body = document.querySelector('body');
+  const [homeNode, menuNode, contactNode] = [Home(), Menu(), Contact()];
+  const hash = {
+    home: homeNode,
+    menu: menuNode,
+    contact: contactNode,
+  };
 
-body.append(Contact());
+  function handleClick(event) {
+    const hashKey = event.target.getAttribute('data-direct');
+    const insetNode = hash[hashKey];
+    body.removeChild(body.lastChild);
+    body.appendChild(insetNode);
+  }
+
+  const tabSwitcher = document.createElement('nav');
+  tabSwitcher.classList.add('flex-horizontal');
+  const homeBtn = document.createElement('button');
+  homeBtn.setAttribute('data-direct', 'home');
+  homeBtn.textContent = 'Home';
+  const menuBtn = document.createElement('button');
+  menuBtn.setAttribute('data-direct', 'menu');
+  menuBtn.textContent = 'Menu';
+  const contactBtn = document.createElement('button');
+  contactBtn.setAttribute('data-direct', 'contact');
+  contactBtn.textContent = 'Contact';
+  const btns = [homeBtn, menuBtn, contactBtn];
+  btns.forEach((btn) => {
+    const store = btn;
+    store.onclick = handleClick;
+  });
+  tabSwitcher.append(...btns);
+
+  body.append(...[tabSwitcher, homeNode]);
+})();
